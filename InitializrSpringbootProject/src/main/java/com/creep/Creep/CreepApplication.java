42af3af95136098399b42dc4546c058883f60593
Ripichip <josueanticona@gmail.com>
2024-07-11 23:38:56 -0600

    Creacion del repositorio

---
+++ InitializrSpringbootProject/src/main/java/com/creep/Creep/CreepApplication.java
@@ -0,0 +1,13 @@
+package com.creep.Creep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreepApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProject/src/test/java/com/creep/Creep/CreepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creep.Creep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
